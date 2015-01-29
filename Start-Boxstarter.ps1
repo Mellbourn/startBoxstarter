589d2df5177162e3b7a8b1be76e31eabede4379b
Klas Mellbourn <klas@mellbourn.net>
2015-01-29 20:02:01 +0100

    new version

--- Start-Boxstarter.ps1
+++ Start-Boxstarter.ps1
@@ -1,1 +1,1 @@
-START http://boxstarter.org/package/url?https://gist.githubusercontent.com/Mellbourn/fa08dd4262201950a395/raw/0ec276c41642fb5e0f04f68d2ba6ce584e1a9610/boxstarterDevMachine.ps1
+START http://boxstarter.org/package/url?https://gist.githubusercontent.com/Mellbourn/fa08dd4262201950a395/raw/7790d188ae848f2b9dd71975b8597a6b725d7e86/boxstarterDevMachine.ps1
